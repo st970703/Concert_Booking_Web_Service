f4875301f39ed84e06e827332cfecf255fe3b1d6
st970703 <st970703@gmail.com>
2017-09-12 22:25:21 +1200

    default service

--- softeng325-concert/softeng325-concert-client/src/main/java/nz/ac/auckland/concert/client/service/DefaultService.java
+++ softeng325-concert/softeng325-concert-client/src/main/java/nz/ac/auckland/concert/client/service/DefaultService.java
@@ -2,6 +2,7 @@ package nz.ac.auckland.concert.client.service;
 
 import java.awt.Image;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicLong;
 
 import nz.ac.auckland.concert.common.dto.BookingDTO;
 import nz.ac.auckland.concert.common.dto.ConcertDTO;
@@ -10,22 +11,50 @@ import nz.ac.auckland.concert.common.dto.PerformerDTO;
 import nz.ac.auckland.concert.common.dto.ReservationDTO;
 import nz.ac.auckland.concert.common.dto.ReservationRequestDTO;
 import nz.ac.auckland.concert.common.dto.UserDTO;
+import nz.ac.auckland.concert.service.services.ConcertResource;
+import nz.ac.auckland.concert.service.services.PersistenceManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.persistence.EntityManager;
+import javax.ws.rs.*;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_XML;
+
+@Path("/concerts")
+@Produces({APPLICATION_XML})
 public class DefaultService implements ConcertService {
 
+	private static Logger _logger = LoggerFactory
+			.getLogger(ConcertResource.class);
+
+	// Declare necessary instance variables.
+	private AtomicLong _idCounter = new AtomicLong();
+
+	private static PersistenceManager pManager = PersistenceManager.instance();
+	private static EntityManager eManager = pManager.createEntityManager();
+
 	@Override
+	@GET
+	@Path("{id}")
+	@Produces({APPLICATION_XML})
 	public Set<ConcertDTO> getConcerts() throws ServiceException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@GET
+	@Path("{id}")
+	@Produces({APPLICATION_XML})
 	public Set<PerformerDTO> getPerformers() throws ServiceException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@POST
+	@Consumes({APPLICATION_XML})
 	public UserDTO createUser(UserDTO newUser) throws ServiceException {
 		// TODO Auto-generated method stub
 		return null;
@@ -38,42 +67,57 @@ public class DefaultService implements ConcertService {
 	}
 
 	@Override
+	@GET
+	@Path("{id}")
+	@Produces({APPLICATION_XML})
 	public Image getImageForPerformer(PerformerDTO performer) throws ServiceException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@POST
+	@Consumes({APPLICATION_XML})
 	public ReservationDTO reserveSeats(ReservationRequestDTO reservationRequest) throws ServiceException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@POST
+	@Consumes({APPLICATION_XML})
 	public void confirmReservation(ReservationDTO reservation) throws ServiceException {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
+	@POST
+	@Consumes({APPLICATION_XML})
 	public void registerCreditCard(CreditCardDTO creditCard) throws ServiceException {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
+	@GET
+	@Path("{id}")
+	@Produces({APPLICATION_XML})
 	public Set<BookingDTO> getBookings() throws ServiceException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@POST
+	@Consumes({APPLICATION_XML})
 	public void subscribeForNewsItems(NewsItemListener listener) {
 		throw new UnsupportedOperationException();
 		
 	}
 
 	@Override
+	@DELETE
 	public void cancelSubscription() {
 		throw new UnsupportedOperationException();
 	}
